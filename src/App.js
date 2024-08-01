3ffd8249cd70b319de4bdff47e3e5d109dfc34b8
Fabrice Bokovi <Fabrice.Bokovi@virtualincision.com>
2024-08-01 10:03:36 -0700

    finished button

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 import MediaControls from './Components/MediaControls/MediaControls';
 import PtzController from './Components/ptz-cam/ptz_controller';
 import { Box } from '@mui/material';
-import SwipeConfirmButton from './Components/SwipeConfirmation/SwipeConfirmButton'
 import SwipeButton from './Components/SwipeButton/SwipeButton';
 import { Margin } from '@mui/icons-material';
 import VolumeSlider from './Components/MediaControls/VolumeSlider';
-import MysteriousText from './Components/TextAnimations/MysteriousText';
 
 function App() {
   return (
@@ -14,9 +12,9 @@ function App() {
       <div style={{ marginTop: '25px', marginLeft: '25px', width: '500px' }}>
         <SwipeButton />
 
-        <MysteriousText>
+        {/* <MysteriousText>
           Lorem ipsum etcertera
-        </MysteriousText>
+        </MysteriousText> */}
         
         {/* <VolumeSlider /> */}
       </div>
--- src/Components/SwipeButton/SwipeButton.js
+++ src/Components/SwipeButton/SwipeButton.js
@@ -8,45 +8,73 @@ import { animated, useSpring } from "react-spring";
 const lerpColor = (color1, color2, factor) => {
     const result = color1.slice();
     for (let i = 0; i < 3; i++) {
-        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i])); 
+        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
     }
     return `rgb(${result.join(',')})`;
 }
 
 const SwipeButton = () => {
     const MIN_BUTTON_POSITION = 8;
-    const MAX_BUTTON_POSITION = 268;
+    const MAX_BUTTON_POSITION = 248;
+    const CONTAINER_WIDTH = 336;
     const MIN_TEXT_POSITION = 134;
 
     const [buttonPosition, setButtonPosition] = useState(MIN_BUTTON_POSITION);
-    const [textPosition, setTextPosition] = useState(MIN_TEXT_POSITION);
     const [isDragging, setIsDragging] = useState(false);
     const [backgroundColor, setBackgroundColor] = useState('#0D1116');
     const [actionComplete, setActionComplete] = useState(false);
     const [actionText, setActionText] = useState("Home Tools")
-    
+    const [isLoading, setIsLoading] = useState(false);
+
     const containerRef = useRef(null);
     const buttonRef = useRef(null);
     const textRef = useRef(null);
-    
-  
+    const currButtonPosRef = useRef(null);
+
+
     const handleTouchStart = (e) => {
-        setIsDragging(true);
+        if (!isLoading) {
+            setIsDragging(true);
+        }
     }
 
     const handleTouchEnd = (e) => {
         setIsDragging(false);
-        if (buttonPosition > MAX_BUTTON_POSITION / 2) {
+        // console.log(buttonPosition);
+        if (currButtonPosRef.current > MAX_BUTTON_POSITION * 0.9) {
             setButtonPosition(MAX_BUTTON_POSITION)
             // additional functionality
             // todo: change text and change icon
             setActionComplete(true);
-            
+            setIsLoading(true);
+
         } else {
+            setIsLoading(false);
             setButtonPosition(MIN_BUTTON_POSITION)
             setBackgroundColor('#0D1116')
         }
     }
+
+    const resetButton = () => {
+        setButtonPosition(MIN_BUTTON_POSITION);
+        setActionComplete(false);
+        setBackgroundColor('#0D1116')
+        setIsLoading(false)
+    }
+
+    // handle loading
+    useEffect(() => {
+        let timer;
+        if (isLoading) {
+            // console.log(isLoading, buttonPosition);
+            timer = setTimeout(() => {
+                resetButton();
+            }, 2000)
+        }
+        return () => {
+            clearTimeout(timer)
+        };
+    }, [isLoading])
 
     // change background color
     useEffect(() => {
@@ -64,29 +92,29 @@ const SwipeButton = () => {
     useEffect(() => {
         if (actionComplete) {
             console.log(actionComplete);
-            setActionText("Homing")
+            // setActionText("Homing")
         }
     }, [buttonPosition])
 
     const handleTouchMove = (e) => {
-        if (!isDragging) return;
+        // console.log(buttonPosition);
+        if (!isDragging || isLoading) return;
         const containerwidth = containerRef.current.offsetWidth;
         const buttonWidth = buttonRef.current.offsetWidth;
-        // const textWidth = textRef.current 
         const maxPosition = containerwidth - buttonWidth - 8;
         const touch = e.touches[0];
         const newButtonPosition = Math.max(MIN_BUTTON_POSITION, Math.min(touch.clientX - containerRef.current.offsetLeft - buttonWidth / 2, maxPosition))
-        const newTextPosition = Math.max(MIN_TEXT_POSITION, Math.min( - textRef.current.offsetLeft - buttonWidth / 2, 900));
+        currButtonPosRef.current = newButtonPosition;
         setButtonPosition(newButtonPosition);
-        setTextPosition(newTextPosition);
+        // console.log(currButtonPosRef.current);
     }
 
     useEffect(() => {
         const container = containerRef.current;
-        
 
-        container.addEventListener('touchstart', handleTouchStart, {passive: true});
-        container.addEventListener('touchmove', handleTouchMove, {passive: true});
+
+        container.addEventListener('touchstart', handleTouchStart, { passive: true });
+        container.addEventListener('touchmove', handleTouchMove, { passive: true });
         container.addEventListener('touchend', handleTouchEnd)
 
         return () => {
@@ -94,12 +122,13 @@ const SwipeButton = () => {
             container.removeEventListener('touchmove', handleTouchMove);
             container.removeEventListener('touchend', handleTouchEnd);
         };
-    }, [isDragging]);
+    }, [isDragging, isLoading]);
 
+    const textPosition = CONTAINER_WIDTH / 2 + (buttonPosition - MIN_BUTTON_POSITION);
 
     return (
         <>
-            <Box 
+            <Box
                 ref={containerRef}
                 id="'swipe-container'"
                 style={{
@@ -114,6 +143,20 @@ const SwipeButton = () => {
                     transition: 'background-color 0.3s ease',
                 }}
             >
+                <Typography
+                    position={"absolute"}
+                    ref={textRef}
+                    color={"white"}
+                    fontSize={24}
+                    fontWeight={'bold'}
+                    style={{
+                        left: `${buttonPosition - 182}px`,
+                        transition: isDragging ? 'none' : 'left 0.3s ease-out',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    Homing
+                </Typography>
                 <Box
                     ref={buttonRef}
                     id='swipe-buttn'
@@ -131,24 +174,32 @@ const SwipeButton = () => {
                     }}
                 >
                     {/* arrow icon */}
-                    <ArrowForwardIcon sx={{ color: 'white', fontSize: '56px'}}/>
+                    {/* <ArrowForwardIcon sx={{ color: 'white', fontSize: '56px' }} /> */}
+                    {isLoading ? (
+                        // Add a loading indicator here, e.g., a spinning icon
+                        <span>Loading...</span>
+                    ) : (
+                        <ArrowForwardIcon sx={{ color: 'white', fontSize: '56px' }} />
+                    )}
 
                 </Box>
-                    <Typography 
-                        position={"absolute"}
-                        ref={textRef}
-                        color={"white"} 
-                        fontSize={24} 
-                        fontWeight={'bold'}
-                        style={{
-                            left: `${textPosition}px`,
-                        }}
-                    > 
-                        {actionText}
-                    </Typography>
+                <Typography
+                    position={"absolute"}
+                    ref={textRef}
+                    color={"white"}
+                    fontSize={24}
+                    fontWeight={'bold'}
+                    style={{
+                        left: `${textPosition}px`,
+                        transition: isDragging ? 'none' : 'left 0.3s ease-out',
+                        whiteSpace: 'nowrap',
+                    }}
+                >
+                    {actionText}
+                </Typography>
             </Box>
-        
-        
+
+
         </>
 
     )
